2860f7002cd81bf686c9cd7880118d13145392b7
egigiffari <egisipayung3@gmail.com>
2024-07-13 00:26:02 +0700

    refactor: add constants

---
+++ constants/error_message.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	ErrUnauthorized   = "RESOURCE_UNAUTHORIZED"
+	ErrForbidden      = "RESOURCE_FORBIDDEN"
+	ErrNotFound       = "RESOURCE_NOT_FOUND"
+	ErrInvalidToken   = "INVALID_TOKEN"
+	ErrInvalidRequest = "INVALID_REQUEST"
+)
